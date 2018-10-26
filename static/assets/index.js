54c9c467b8c6887edecb6358a88cf33d97ec0446
Manuel <manuel.c1992@gmail.com>
2018-10-26 16:08:14 -0500

    Add table with VRM

--- static/assets/index.js
+++ static/assets/index.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
 	function search() {
 		var alertErrorCabins = $('#alert-danger-cabins')
 		var progressBarCabins = $('#progress-cabins')
+
+		var alertErrorVRM = $('#alert-danger-vrm')
+		var progressBarVRM = $('#progress-vrm')
+
 		progressBarCabins.removeClass('d-none')
 		alertErrorCabins.addClass('d-none')
+		progressBarVRM.removeClass('d-none')
+		alertErrorVRM.addClass('d-none')
 
 		$.get('/api/cabins')
-			.done(function( data ) {
+			.then(function( data ) {
 				progressBarCabins.addClass('d-none')
 				$('#cabins').DataTable( {
 					data: data,
@@ -18,7 +24,7 @@ $(document).ready(function() {
 						{
 							title: "DESCRIPTION",
 							render: function ( data, type, row ) {
-								return `<span class="d-inline-block text-truncate" data-toggle="tooltip" style="max-width: 150px;" title="${data}">
+								return `<span class="d-inline-block text-truncate" style="max-width: 150px;" title="${data}">
 								${data}
 							  </span>`;
 							}
@@ -31,11 +37,26 @@ $(document).ready(function() {
 						{ title: "STATUS" }
 					]
 				} );
-				$('[data-toggle="tooltip"]').tooltip();
+				return $.get('/api/vrm');
+			})
+			.done(function (data) {
+				progressBarVRM.addClass('d-none')
+				$('#vrm').DataTable( {
+					data: data,
+					columns: [
+						{ title: "IDVRM" },
+						{ title: "NAME" },
+						{ title: "WEBSITE" },
+						{ title: "CABINS" },
+						{ title: "LAST SCRAPE" }
+					]
+				} );
 			})
 			.fail(function() {
 				progressBarCabins.addClass('d-none')
 				alertErrorCabins.removeClass('d-none')
+				progressBarVRM.addClass('d-none')
+				alertErrorVRM.removeClass('d-none')
 			})
 	}
 	search();
