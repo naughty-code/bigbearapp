5629250e221cb913c4c71a61e6f4fd20550f0d48
naughty-code <javier.murar@gmail.com>
2018-10-26 16:31:36 -0400

    changed client ajax routes

--- server.py
+++ server.py
@@ -10,23 +10,27 @@ load_dotenv()
 app = Flask(__name__, static_url_path='')
 CORS(app)
 
-@app.route('/api')
+@app.route('/api/cabins')
 def api():
     connection = psycopg2.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USERNAME'), password=os.getenv('DB_PASSWORD'), host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'))
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM db.cabin')
     data = cursor.fetchall()
+    cursor.close()
+    connection.close()
     return jsonify(data)
 
 @app.route('/api/vrm')
 def vrm():
+    connection = psycopg2.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USERNAME'), password=os.getenv('DB_PASSWORD'), host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'))
+    cursor = connection.cursor()
     cursor.execute('SELECT * FROM db.vrm')
     data = cursor.fetchall()
+    cursor.close()
+    connection.close()
     return jsonify(data)
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
-cursor.close()
-connection.close()
