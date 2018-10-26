b8120adec379c6ad25e12c736b0f5629f43bc260
Manuel <manuel.c1992@gmail.com>
2018-10-26 13:22:02 -0500

    Create .env file with environment vars

--- server.py
+++ server.py
@@ -3,14 +3,13 @@ import psycopg2
 from flask_cors import CORS
 from flask import jsonify
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
+DATABASE_URL = os.environ['DATABASE_URL']
 
 app = Flask(__name__, static_url_path='')
 CORS(app)
 
-connection = psycopg2.connect(dbname='d4pp6fmbq2237a', user='hseahwenpwiswg', password='2e56524af38f5e01ed406d57e3631d05782128a2239fca9ca7ebeb05014cb638', host='ec2-184-73-169-151.compute-1.amazonaws.com', port='5432')
+connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = connection.cursor()
 
 @app.route('/api/cabins')
@@ -28,6 +27,3 @@ def vrm():
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
-
-cursor.close()
-connection.close()
